0c9493f82d631b04b119401b9c7bac7675f79324
mjedwards <jairedwards20@gmail.com>
2019-12-15 21:32:01 -0500

    added initial tests

--- baseball-react/src/App.js
+++ baseball-react/src/App.js
@@ -4,9 +4,10 @@ import "./App.css";
 import Display from "./components/Display";
 import Dashboard from "./components/Dashboard";
 function App() {
+  const [reset, setReset] = useState(0);
   const [balls, setBalls] = useState(0);
   const countBalls = () => {
-    setBalls(balls => balls + 1, balls >= 4 ? setBalls(-1) : balls);
+    setBalls(balls => balls + 1, balls === 4 ? setBalls(-1) : balls);
   };
 
   const [strikes, setStrikes] = useState(0);
@@ -16,14 +17,24 @@ function App() {
 
   const [fouls, setFouls] = useState(0);
   const countFouls = () => {
-    setFouls(fouls => fouls + 1, fouls >= 2 ? setFouls(-1) : fouls);
+    setFouls(
+      fouls => fouls + 1,
+      fouls >= 2 ? setFouls(-1) : fouls,
+      fouls ? setStrikes(strikes => strikes + 1) : fouls,
+      strikes === 2 ? setStrikes(strikes) : fouls
+    );
   };
 
   const [hits, setHits] = useState(0);
   const countHits = () => {
-    setHits(Hits => Hits + 1);
+    setHits(
+      Hits => Hits + 1,
+      hits >= 1 ? setStrikes(reset) : hits,
+      hits >= 1 ? setBalls(reset) : hits
+    );
   };
 
+  console.log(strikes);
   return (
     <div className='App'>
       <Display balls={balls} strikes={strikes} />
--- baseball-react/src/App.test.js
+++ baseball-react/src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(rtl.cleanup);
+it("renders without crashing", () => {});
+it("renders elements", () => {
+  const wrap = rtl.render(<App />);
+
+  console.log(wrap.debug());
+});
+
+it("renders Display", () => {
+  const wrap = rtl.render(<div>Display</div>);
+  const element = wrap.queryByText(/Display/i);
+  console.log(wrap.debug());
+
+  expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
 });
